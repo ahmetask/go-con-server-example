e7b06f5a10042fee4026468408099b2aac92d10e
Ahmet Aşık <ahmet9417@gmail.com>
2020-09-28 17:26:44 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 docker build -t go-con-server .
 kubectl delete deployment go-con-server
 kubectl delete service go-con-server
-kubectl apply -f deployment.yml
-kubectl apply -f service.yml
+kubectl create -f deployment.yml
+kubectl create -f service.yml
 kubectl apply -f role.yml
 kubectl apply -f rolebinding.yml
 kubectl apply -f cluster_role.yml
